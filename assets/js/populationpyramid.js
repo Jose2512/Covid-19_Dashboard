cc537f396c5bcc25601cd92f07b9dc4d93799015
Jose Alberto Alvarez <josealvarezhuerta@me.com>
2020-06-27 13:00:47 -0500

    Actualización al index con graficas

--- assets/js/populationpyramid.js
+++ assets/js/populationpyramid.js
@@ -1,10 +1,10 @@
 
 // data must be in a format with age, male, and female in each object
 var exampleData = d3.json('../assets/data/genero_casos.json').then(function(data){
-    pyramidBuilder(data, '#charMW', {height: 400, width: 600});
+    pyramidBuilder(data, '#charMW', {height: 400, width: 500});
 })
 
 var exampleData = d3.json('../assets/data/genero_difuntos.json').then(function(data){
-    pyramidBuilder(data, '#charMW_dec', {height: 400, width: 600});
+    pyramidBuilder(data, '#charMW_dec', {height: 400, width: 500});
 })  
 
